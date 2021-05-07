b1205e48d6b89cb4eaffe00b400d093d30ef5ca4
Kevin Huynh <kevinhuynh269@yahoo.com>
2021-05-07 12:32:19 -0500

    #14 created a history component and added it to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './components/history/history.component';
 import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
 
 const routes: Routes = [
   {
     path: 'vehiclelist',
     component: VehicleListComponent
+  },
+  {
+    path: 'history',
+    component: HistoryComponent
   }
 ];
 
