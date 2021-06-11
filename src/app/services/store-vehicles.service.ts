e559b932a488160d59154053363379edd7e2f90b
psalaza <43583947+psalaza@users.noreply.github.com>
2021-06-11 13:55:17 -0400

    fixed errors on tests

--- src/app/services/store-vehicles.service.ts
+++ src/app/services/store-vehicles.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
-import ApiResponse from '../shared/ApiResponse';
+//import ApiResponse from '../shared/ApiResponse';
 import { Observable, of } from 'rxjs';
 import { VehicleBasic } from '../models/vehicle-basic';
 import { VehicleResponse } from '../models/vehicle-response';
@@ -26,9 +26,9 @@ export class StoreVehiclesService {
   * information from the endpoint and      *
   * return the data as an observable       *
   *****************************************/
-  getAll() : Observable<ApiResponse> {
+  getAll() : Observable<VehicleResponse[]> {
       // get<ApiReponse>(endpoint)
-      return this.http.get<ApiResponse>(this.endpoint)
+      return this.http.get<VehicleResponse[]>(this.endpoint)
       .pipe(
         tap(
           success => {this.status = success},
