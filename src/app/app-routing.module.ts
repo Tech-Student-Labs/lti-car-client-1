9ffa93c5c21acf62c30eeedfdcc5f1f2d1456140
Steven Pazur <sjpazur@yahoo.com>
2021-06-13 09:58:01 -0700

    Fixed home page thing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HistoryComponent } from './components/history/history.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginpageComponent } from './components/loginpage/loginpage.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { SubmitVehicleComponent } from './components/submit-vehicle/submit-vehicle.component';
 import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'vehiclelist',
     component: VehicleListComponent
