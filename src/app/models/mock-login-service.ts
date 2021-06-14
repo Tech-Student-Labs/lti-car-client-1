3637c0852ce39a3a429c0e8d4d3a549f189a9a29
Steven Pazur <sjpazur@yahoo.com>
2021-06-14 14:03:11 -0700

    added new injectable thing

--- src/app/models/mock-login-service.ts
+++ src/app/models/mock-login-service.ts
@@ -1,8 +1,12 @@
+import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 import LOGIN from '../../assets/login.json';
 import { LoginService } from "../services/login.service";
 import { UserLogin } from "./user-login";
 
+@Injectable({
+    providedIn: 'root'
+  })
 export class MockLoginService extends LoginService {
     Login(user: UserLogin): Observable<any> {
         return of(LOGIN);
--- src/app/models/mock-vehicle-service.ts
+++ src/app/models/mock-vehicle-service.ts
@@ -2,7 +2,11 @@ import { StoreVehiclesService } from "../services/store-vehicles.service";
 import CARS from '../../assets/vehicle.json';
 import { Observable, of } from "rxjs";
 import { VehicleResponse } from "./vehicle-response";
+import { Injectable } from "@angular/core";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class MockVehicleService extends StoreVehiclesService {
   getAll(): Observable<any>{
     return of(CARS);
