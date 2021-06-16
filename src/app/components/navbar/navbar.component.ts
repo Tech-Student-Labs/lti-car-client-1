687b332e94ab1af27af72b3e7bfe10453d05dc43
RexBrown122 <rexrobert1000@gmail.com>
2021-06-16 11:03:53 -0500

    added logout button logic

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   public userLoggedIn: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigateByUrl('');
+    document.getElementById('login')!.style.display = "block";
+    document.getElementById('signup')!.style.display = "block";
+    document.getElementById('logout')!.style.display = "none";
   }
 }
